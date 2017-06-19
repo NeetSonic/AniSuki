4807f9e5aed51e252a8366ae0d61343c4c2407a8
neetsonic <neetsonic@gmail.com>
2017-06-19 22:03:56 +0800

    dev

--- AniSuki/Util/DataAccess.cs
+++ AniSuki/Util/DataAccess.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using AniSuki.Model;
 using Neetsonic.Tool.Database;
@@ -15,24 +16,29 @@ namespace AniSuki.Util
             return SqlExecutor.ConnectSucceed(ConnectionString);
         }
 
-        private static void ExecuteNonQuery(string sql)
+        private static void ExecuteNonQuery(string sql, SqlParameter[] parameters = null)
         {
-            SqlExecutor.ExecuteNonQuery(ConnectionString, sql);
+            SqlExecutor.ExecuteNonQuery(ConnectionString, sql, parameters);
         }
 
-        private static object ExecuteScalar(string sql)
+        private static object ExecuteScalar(string sql, SqlParameter[] parameters = null)
         {
-            return SqlExecutor.ExecuteScalar(ConnectionString, sql);
+            return SqlExecutor.ExecuteScalar(ConnectionString, sql, parameters);
         }
 
-        private static DataSet ExecuteQuery(string sql)
+        private static DataSet ExecuteQuery(string sql, SqlParameter[] parameters = null)
         {
-            return SqlExecutor.ExecuteQuery(ConnectionString, sql);
+            return SqlExecutor.ExecuteQuery(ConnectionString, sql, parameters);
         }
 
         public static IEnumerable<Producer> GetProducer()
         {
             return from DataRow dr in ExecuteQuery(@"SELECT * FROM Producer").Tables[0].Rows select Producer.FromDataRow(dr);
+        }
+
+        public static void Test(string name)
+        {
+            ExecuteNonQuery(@"INSERT INTO Producer(Name) VALUES(@name)", new []{new SqlParameter(nameof(name), SqlDbType.NVarChar)});
         }
     }
 }
